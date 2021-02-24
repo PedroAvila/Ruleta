0e5e272d07b9e894fd274466caf5ba33c7ce0f86
PedroAvila <minimainor_austin@hotmail.com>
2021-02-24 11:52:21 -0500

    .......

--- TecSoftware.Persistencia/Repositories/RepositoryRuleta.cs
+++ TecSoftware.Persistencia/Repositories/RepositoryRuleta.cs
@@ -5,7 +5,11 @@ using TecSoftware.EntidadesDominio;
 namespace TecSoftware.Persistencia
 {
     public class RepositoryRuleta : IRuleta<Ruleta>
-    {
+    {/// <summary>
+    /// /
+    /// </summary>
+    /// <param name="entity"></param>
+    /// <returns></returns>
         public Task Create(Ruleta entity)
         {
             throw new NotImplementedException();
